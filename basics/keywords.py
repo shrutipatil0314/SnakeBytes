b79b26d2c3cefe9044394d101241427af5f8b2b6
shruti <shrutipatil03141288@gmail.com>
2026-04-03 20:37:49 +0530

    basics.py

---
+++ basics/keywords.py
@@ -0,0 +1,15 @@
+import keywords 
+keywords_list = keywords.kwlist
+print(keywords_list)
+
+
+#value -#['False', 'None', 'True']
+#logical operators -#'and', 'or', 'not' ,'is', 'in', 'not in']
+#control flow -#'if', 'elif', 'else', 'with']
+#loops -#'for', 'while','break', 'continue']
+#functions and classes -#'def', 'lambda','return', 'yield' ]
+#classes and objects -#'class', 'self', 'super']
+#exception handling -#'try', 'except', 'finally', 'raise','assert']
+#import and modules -#'import', 'from', 'as']
+#variables and scope -#'global', 'nonlocal']
+#context?asynchronous programming -#'async', 'await','with']
