6c0f2da958063f9a9b70fb1d3a643f5242ad5870
shruti <shrutipatil03141288@gmail.com>
2026-04-04 15:32:11 +0530

    basics.py

---
+++ basics/datatype.py
@@ -0,0 +1,22 @@
+# DataTypes in Python 
+
+# Integer
+x = 5
+print(type(x))
+
+# Float
+y = 5.0
+print(type(y))
+
+# String
+z = "Hello, World!"
+print(type(z))
+
+# Boolean
+a = True
+print(type(a))  
+
+#none
+b = None
+print(type(b))
+
