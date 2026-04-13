3d16c21d09a99f2443a31eba28dff214a2070c6b
shruti <shrutipatil03141288@gmail.com>
2026-04-13 21:19:49 +0530

    Initial commit

--- basics/Arithmeticandoperators.py
+++ basics/Arithmeticandoperators.py
@@ -14,77 +14,50 @@
 #Arithmetic Operators
  
 print(5 + 3)  # Addition
-print(5 - 3)  # Subtraction
-print(5 * 3)  # Multiplication
-print(5 / 3)  # Division
 
+print(5 - 3)  # Subtraction
+
+print(5 * 3)  # Multiplication
+
+print(5 / 3)  # Division
 #give float value
 
 print(5 // 3) # Floor Division
+
 print(5 % 3)  # Modulus
 #gives the remainder of the division
+
 print(5 ** 3) # Exponentiation 
 #5*5*5
 
-#Operator Precedence
+#Assignment Operator
 
-print(5 + 3 * 2) # Multiplication is done before addition
-print((5 + 3) * 2) # Parentheses change the order of operations
+x = 5
+x += 3  # Equivalent to x = x + 3
+print(x)  # Output: 8
 
+x -= 2  # Equivalent to x = x - 2
+print(x)  # Output: 6
 
-#Comparison Operators
+x *= 4  # Equivalent to x = x * 4
+print(x)  # Output: 24
 
-print(5 > 3)  # Greater than
-print(5 < 3)  # Less than
-print(5 >= 3) # Greater than or equal to
-print(5 <= 3) # Less than or equal to
-print(5 == 3) # Equal to
-print(5 != 3) # Not equal to
+x /= 6  # Equivalent to x = x / 6
+print(x)  # Output: 4.0
 
+x //= 2  # Equivalent to x = x // 2
+print(x)  # Output: 2.0
 
+x %= 2  # Equivalent to x = x % 2
+print(x)  # Output: 0.0
 
-#Logical Operators
-
-print(True and False)  # Logical AND
-print(True or False)   # Logical OR
-print(not True)       # Logical NOT
-
-
-
-#Bitwise Operators
-
-print(5 & 3)  # Bitwise AND
-print(5 | 3)  # Bitwise OR
-print(5 ^ 3)  # Bitwise XOR
-print(~5)     # Bitwise NOT
-print(5 << 1) # Left Shift
-print(5 >> 1) # Right Shift
+x **= 3  # Equivalent to x = x ** 3
+print(x)  # Output: 0.0
 
 
-#Identity Operators
 
-a = [1, 2, 3]
-b = a  # b references the same list as a
-c = [1, 2, 3]  # c is a different list with the same content
-print(a is b)  # True, because a and b reference the same object
-print(a is c)  # False, because a and c reference different objects
-print(a == c)  # True, because a and c have the same content
 
-#Membership Operators
 
-my_list = [1, 2, 3, 4, 5]    
-
-print(3 in my_list)  # True, because 3 is in the list
-print(6 in my_list)  # False, because 6 is not in the list
-print(3 not in my_list)  # False, because 3 is in the list
-print(6 not in my_list)  # True, because 6 is not in the list
-
-#Ternary Operator   
-
-age = 18
-status = "Adult" if age >= 18 else "Minor"
-
-print(status)  # Output: Adult
 
 
 
