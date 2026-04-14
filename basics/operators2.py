01f22654ec57c96d8d3e5092bbc8c30791555dbe
shruti <shrutipatil03141288@gmail.com>
2026-04-14 15:09:32 +0530

    Initial commit

---
+++ basics/operators2.py
@@ -0,0 +1,23 @@
+#//comparison operators\\
+
+a = 10
+b = 20
+
+print(a == b) #False
+# ??checks if a and b are equal
+
+print(a != b) #True
+# ??checks if a and b are not equal
+
+print(a > b)  #False
+# ??checks if a is greater than b
+
+print(a < b)  #True
+# ??checks if a is less than b
+
+print(a >= b) #False
+#?? checks if a is greater than or equal to b
+
+print(a <= b) #True
+#?? checks if a is less than or equal to b
+
