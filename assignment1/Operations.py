0dfe33178bf6f9929928f738ccb0f8d0cb13db61
shruti <shrutipatil03141288@gmail.com>
2026-04-21 13:15:47 +0530

    changed

---
+++ assignment1/Operations.py
@@ -0,0 +1,15 @@
+#||** Perform Basic Mathematical Operations **||
+
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+
+a = num1 + num2
+s = num1 - num2
+m = num1 * num2
+d = num1 / num2
+
+print("Addition:",a)
+print("Subtraction:",s)
+print("Multiplication:",m)
+print("Division:",d)
