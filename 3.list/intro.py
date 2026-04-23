30aba85364ff8d3ba288317ae9454541b058bac6
shruti <shrutipatil03141288@gmail.com>
2026-04-23 15:51:43 +0530

    new file

---
+++ 3.list/intro.py
@@ -0,0 +1,20 @@
+#||     introduction to list     ||
+#^^   list is a collection of items in a particular order
+#^^     list is mutable (can be changed)
+#^^     list is defined by square brackets []
+#^^     list can contain different types of data
+
+#*creating a list*
+my_list = [1, 2, 3, "hello", True]
+print(my_list)  #& Output: [1, 2, 3, 'hello', True]
+print(type(my_list))  #& Output: <class 'list'>
+
+#length of a list
+print(len(my_list))  #& Output: 5
+print(len([]))  #& Output: 0    
+
+print(list("hello"))  #& Output: ['h', 'e', 'l', 'l', 'o']
+print(list((1, 2, 3)))  #& Output: [1, 2, 3]
+print(list({1: "one", 2: "two"}))  #& Output: [1, 2]
+
+
