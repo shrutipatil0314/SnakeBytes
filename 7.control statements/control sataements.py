763efe00ba2e8a6ce0e8862427948986461ebb6c
shruti <shrutipatil03141288@gmail.com>
2026-05-01 14:04:38 +0530

    new file

---
+++ 7.control statements/control sataements.py
@@ -0,0 +1,30 @@
+#||  Conditional Statements (Decision Making)  ||
+
+#^Conditional statements allow your program to execute different blocks of code based on
+#^whether a certain condition is True or False. This is how your programs "make decisions."
+
+#^ Before we make decisions, we need to be able to compare values. Relational operators (also
+#^ known as comparison operators) are used to compare two values and evaluate to either True
+#^ or False. These True or False values (known as Booleans) are the foundation of all
+#^ decision-making in programming.
+#^ Operator Meaning Console
+
+
+# x = 10
+# y = 7
+#x == y
+#False
+# x != y
+#True
+# x > y
+#True
+# x < y
+#False
+# x >= 10
+#True
+# y <= 7
+#rue
+#"apple" == "orange"
+#alse
+#"Python" != "python" #!Case sensitivity matters!
+#rue
