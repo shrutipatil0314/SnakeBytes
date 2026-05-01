0482abba7a24e2e02273b1b40327959a9d71200c
shruti <shrutipatil03141288@gmail.com>
2026-05-01 14:27:14 +0530

    new file

---
+++ 7.control statements/1.the if block.py
@@ -0,0 +1,21 @@
+#|| The if Statement ||
+
+#^ The if statement is the simplest decision-making statement. It executes a block of code only
+#^if a specified condition evaluates to True.
+#* indentation but the spaces to create block
+#*block is a multiple lines of code 
+age = float(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+
+print("welcome")
+
+age2= float(input("Enter your age: "))
+if age2 <= 18:
+   print("You are an child")
+print("welcome")
+
+
+
+# Nothing is printed because the condition (16 >= 18) is False
+
