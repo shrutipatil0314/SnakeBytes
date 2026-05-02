8e6c4c790047c1c8365cf96f4b92781bb0b92515
shruti <shrutipatil03141288@gmail.com>
2026-05-02 23:06:59 +0530

    new fil

--- 8.loop/2.for loop.py
+++ 8.loop/2.for loop.py
@@ -1,5 +1,21 @@
 #|| loops ||
 """
  it is an iterator based loop which steps through the items of a collection 
-(lists , tuples , sets, dict, str)
+(lists , tuples , sets, dict, str) , and  executes a block od code repeatedly 
+for a number of time equal to the number of items in the collection
 """
+
+#*for var in sequence:
+#     statement 1
+#     statement2
+#     .........
+#     statement n
+
+percents = [66.5 , 83.2 , 86.4 , 97.3]
+print(percents[0])
+print(percents[1])
+print(percents[2])
+print(percents[3])
+#                      ^   2 diff method 
+for x in percents:
+    print(x)
