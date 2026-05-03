39dcbbaba42641a5242adb7ecf790e6e7078a4ba
shruti <shrutipatil03141288@gmail.com>
2026-05-03 12:11:04 +0530

    new

---
+++ assignment2/task1.py
@@ -0,0 +1,11 @@
+num1 =(input("Enter a number:"))
+n= int(num1)
+
+store_value=0
+
+for currnt_value in range(1,n+1):
+    store_value+=currnt_value
+
+
+print(f"\n sum of all number for 1 to {n} is {store_value}")
+
---
+++ assignment2/task2.py
@@ -0,0 +1,7 @@
+num1= (int(input("Enter a number: ")))
+
+if num1 %2 == 0:
+    print(num1,"The number is even")
+else:
+    print(num1,"The number is odd")
+
