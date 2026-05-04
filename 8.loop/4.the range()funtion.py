b0d58df8c4b57ad3039da2dc438abcba7c1a7fa8
shruti <shrutipatil03141288@gmail.com>
2026-05-04 23:26:22 +0530

    new

---
+++ 8.loop/4.the range()funtion.py
@@ -0,0 +1,18 @@
+#|| the range () funtion 
+
+"""
+range () - bulit - in funtion used to generate sequence of integers in a given int 
+range (start , stop , step ) stop is not included
+
+^ for i in range (start, stop, step):
+      #statements
+"""
+
+for i in range(1,11,3): # 1 , 2, 3, .... 8,9
+    pass
+#    print(i)
+
+#^ generate even numbers between 1 and 10 (10 exclued)
+
+for i in range(2,10,2):
+    print(i)
