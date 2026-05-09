8769c7a153325c46154e1c8309b82d23e2c66986
shruti <shrutipatil03141288@gmail.com>
2026-05-09 22:52:23 +0530

    new

--- 8.loop/8.infinite while loop.py
+++ 8.loop/8.infinite while loop.py
@@ -17,3 +17,41 @@ for i in range(1, 11):
     if i==5:
         break
     print(i)
+
+#|| the while loop ||
+"""
+The while loop is used to execute a block of code repeatedly as long as a certain condition is true.
+The syntax of a while loop is as follows:
+while condition:
+    # code to be executed
+"""
+
+#|| example of while loop ||
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+
+
+#|# example of while loop with else ||
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+else:
+    print("i is greater than 5")
+    print("end of loop")
+
+
+#
+#|| example of while loop with break ||
+i=1
+while i<=5:
+    if i==3:
+        break
+    print(i)
+    i=i+1
+else:
+    print("i is greater than 5")
+    print("end of loop")
+    
