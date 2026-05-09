88c58162d81f815e5a85831498a37d165504d779
shruti <shrutipatil03141288@gmail.com>
2026-05-09 22:49:36 +0530

    new

--- 8.loop/6.contitnueandbreak.py
+++ 8.loop/6.contitnueandbreak.py
@@ -18,4 +18,3 @@ for i in range(1, 11):
         break
     print(i)
 
-    
---
+++ 8.loop/8.infinite while loop.py
@@ -0,0 +1,19 @@
+#|| continue and break ||
+
+"""
+continue and break are used to control the flow of a loop.
+continue skips the current iteration and moves to the next one.
+break exits the loop entirely.
+"""
+
+#|| continue ||
+for i in range(1, 11):
+    if i%2==0:
+        continue
+    print(i)
+
+#|| break ||
+for i in range(1, 11):
+    if i==5:
+        break
+    print(i)
