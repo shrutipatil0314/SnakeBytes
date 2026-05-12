80fa9288dbdde0a4de09992aa27d6985b1b38944
shruti <shrutipatil03141288@gmail.com>
2026-05-12 15:36:22 +0530

    new

---
+++ 8.loop/11.star pattern.py
@@ -0,0 +1,17 @@
+#|| star parttern ||
+
+#^for i in range (1, 6):
+ #   for j in range (1,i*1):
+  #      print("*")
+
+#^for i in range (1, 6):
+#    for j in range (1,i+1):
+ #       print("*", end="")
+    
+for i in range(1, 6):
+    for j in range(1,i+1):
+        print('*', end=" ")
+    print()
+
+
+
