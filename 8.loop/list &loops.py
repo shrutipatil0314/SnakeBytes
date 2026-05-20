470a9e8a9e2eb9e544ff15d152cf3adc52597019
shruti <shrutipatil03141288@gmail.com>
2026-05-20 22:23:41 +0530

    new

--- 8.loop/list &loops.py
+++ 8.loop/list &loops.py
@@ -1,37 +1,14 @@
-#|| the while loop ||
-"""
-The while loop is used to execute a block of code repeatedly as long as a certain condition is true.
-The syntax of a while loop is as follows:
-while condition:
-    # code to be executed
-"""
+countries = ["India", "USA", "UK", "Australia", "Germany",
+              "Ireland", "Italy", "Spain", "France", "Russia"]
 
-#|| example of while loop ||
-i=1
-while i<=5:
-    print(i)
-    i=i+1
-
-
-#|# example of while loop with else ||
-i=1
-while i<=5:
-    print(i)
-    i=i+1
-else:
-    print("i is greater than 5")
-    print("end of loop")
-
-
-#
-#|| example of while loop with break ||
-i=1
-while i<=5:
-    if i==3:
-        break
-    print(i)
-    i=i+1
-else:
-    print("i is greater than 5")
-    print("end of loop")
+#count all the countries whic are starting with letter 'I'
+#also print the count at the end
+count = 0
+output = []
+for country in countries:
+    if country.startswith("I"):
+        count=count+1
+        output.append(country)
+print(output)
+print(count)
     
