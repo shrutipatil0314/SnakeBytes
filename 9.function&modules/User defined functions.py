615f81cedf05b7b7da777006d6ffad8173be6953
shruti <shrutipatil03141288@gmail.com>
2026-06-19 19:14:22 +0530

    Your commit message

--- 9.function&modules/User defined functions.py
+++ 9.function&modules/User defined functions.py
@@ -9,3 +9,14 @@ greeting_someone("Alice")
 greeting_someone("Bob")
 greeting_someone("Charlie")
 
+
+#example 2
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("is an even number.")
+    else:
+        print("is ot an even number.")
+
+
+even_or_odd(4)
+even_or_odd(7)
