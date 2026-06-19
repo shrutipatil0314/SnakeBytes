7b07f8f3eee36fb704aee303d2c04998d9812cdb
shruti <shrutipatil03141288@gmail.com>
2026-06-19 19:02:20 +0530

    Your commit message

---
+++ 9.function&modules/User defined functions.py
@@ -0,0 +1,11 @@
+# || User defined functions  ||
+
+def greeting_someone(name):
+    print(f"Hello, {name}! How are you today?")
+    print("I hope you're having a great day!")
+
+#calling the function
+greeting_someone("Alice")
+greeting_someone("Bob")
+greeting_someone("Charlie")
+
