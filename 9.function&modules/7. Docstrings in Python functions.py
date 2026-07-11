0af369efe54d06293f7b9014aa4d8ae17d52a2e2
shruti <shrutipatil03141288@gmail.com>
2026-07-11 12:09:01 +0530

    new

---
+++ 9.function&modules/7. Docstrings in Python functions.py
@@ -0,0 +1,39 @@
+def divide(num1, num2):
+    """
+    num1: a number to be divided (numerator)
+    num2: a number to be divided by (denominator)
+    return: the result of the division of num1 by num2
+    """
+    if num2 == 0:
+        return "Error: Division by zero is not allowed."
+    else:
+        return num1 / num2
+        return result
+    
+print(divide(num1=10, num2=0))  # Output: 5.0
+print(help(divide))  # Output: Displays the docstring for the divide function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
