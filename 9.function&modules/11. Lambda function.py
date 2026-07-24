3e9a0c550a6088ffa944c2f04ba8a7e6cf0ff888
shruti <shrutipatil03141288@gmail.com>
2026-07-24 14:42:48 +0530

    new

---
+++ 9.function&modules/11. Lambda function.py
@@ -0,0 +1,17 @@
+# // lambda function //
+
+"""
+A lambda function is a small anonymous function that can take any number of arguments but can only have one expression.
+
+def add (a,b    ):
+    return a + b
+res=add(2,3)
+print(res)
+
+"""
+#syntax of lambda function
+#lambda arguments : expression
+
+add = lambda a, b : a + b
+res = add(2,9)
+print(res)
