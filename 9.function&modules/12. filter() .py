9d311940b86ada006c1fe79c3853a76920bb4d4b
shruti <shrutipatil03141288@gmail.com>
2026-07-25 00:02:03 +0530

    new

---
+++ 9.function&modules/12. filter() .py
@@ -0,0 +1,7 @@
+# // filter (funtion, sequence) -> filter object
+#  filter() function is used to filter the given sequence with the help of a function that tests 
+# each element in the sequence to be true or not. 
+
+sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+filtered_result = filter(lambda x:  True if x % 2 != 0 else False, sequence)
+print(list(filtered_result))
