728bbf1442f20984f1d9b99059fdc88e2c3cf766
shruti <shrutipatil03141288@gmail.com>
2026-07-25 12:12:37 +0530

    new

--- 9.function&modules/12. filter() .py
+++ 9.function&modules/12. filter() .py
@@ -4,4 +4,5 @@
 
 sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 filtered_result = filter(lambda x:  True if x % 2 != 0 else False, sequence)
-print(list(filtered_result))
+print(filtered_result)
+print(f"odd number in the list are : {list(filtered_result)}")
