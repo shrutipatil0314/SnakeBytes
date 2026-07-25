b9febdc8d97e20ff8f463067c758dbd866cd5111
shruti <shrutipatil03141288@gmail.com>
2026-07-25 12:24:45 +0530

    new

---
+++ 9.function&modules/12. map() with lambda.py
@@ -0,0 +1,8 @@
+# // map (function, iterable) -> map object
+#  map() function is used to apply a given function to each item of an iterable 
+# (list, tuple etc.) and returns a map object.
+
+sequence = [1, 2, 3, 4, 5]
+mapped_result = map(lambda x: x * 2, sequence)
+print(mapped_result)
+print(f"double of each number in the list are : {list(mapped_result)}")
