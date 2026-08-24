2e0e1ba2d0f95859e7ab89a68759735992653aa8
shruti <shrutipatil03141288@gmail.com>
2026-08-24 13:51:36 +0530

    new

--- 10.file & exeption handling/13. The pickle module/pickle_module.py
+++ 10.file & exeption handling/13. The pickle module/pickle_module.py
@@ -10,3 +10,12 @@ print(type(students))
 with open('students.pkl', 'bx') as fh:
     for student in students:
         pickle.dump(students[student], fh)
+
+#* Deserializing the dictionary using pickle
+with open('students.pkl', 'rb') as fh:
+    data1 = pickle.load(fh)
+    print(data1,type(data1))
+    data2 = pickle.load(fh)
+    print(data2,type(data2))
+    data3 = pickle.load(fh)
+    print(data3,type(data3))
