5f343f0745e425996292239e9c03a10cc48aee8d
shruti <shrutipatil03141288@gmail.com>
2026-09-02 13:16:33 +0530

    new

--- 11. regular expression/1. Introduction to RegEx.py
+++ 11. regular expression/1. Introduction to RegEx.py
@@ -14,3 +14,15 @@ print("Length of the message:", len(message))
 print(message.find("welcome"))  #* Find the index of the substring "welcome" in the message
 print(message.find("Python"))  #* Find the index of the substring "Python" in the message (not found, returns -1)
 
+"""
+# re.search(regex_pattern, string) is a function in the `re` module that searches for a specified pattern (regex_pattern) in a given string.
+# If the pattern is found, it returns a match object; otherwise, it returns None.
+"""
+
+import re  #^ Import the `re` module for regular expressions
+
+message = "Hello , welcome to the world of regular expressions!" 
+
+match_obj = re.search(r"welcome", message)  #* Search for the substring "welcome" in the message using a regular expression
+print(match_obj)  #* Print the match object if found, otherwise None
+
